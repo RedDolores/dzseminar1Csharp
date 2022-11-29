091b4bc6517cb7ee0f6345c46ce518175ac317a9
Dolores <dee12098@yandex.ru>
2022-11-29 22:50:54 +0300

    complete dz1

---
+++ dz1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите 1 число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2) Console.WriteLine(num1);
+else Console.WriteLine(num2);
