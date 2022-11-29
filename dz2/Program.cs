0e481de78b29adaa5dd7646a8e7ee33c9939d3d0
Dolores <dee12098@yandex.ru>
2022-11-29 23:09:41 +0300

    complete dz2

---
+++ dz2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите 1 число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 3 число: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if(num2 > max) max = num2;
+if(num3 > max) max = num3;
+Console.WriteLine(max);
